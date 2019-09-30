45a3ed29853cbf61678129a6f223b7fa90e2e599
AlexanderRichey <alexander.richey@gmail.com>
2019-09-30 00:02:05 -0700

    add sentry

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
 
 import App from "./components/App";
+
+Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DSN });
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
