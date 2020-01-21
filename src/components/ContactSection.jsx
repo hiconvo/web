12d69749319b522366e0274862dee02656f2ebd4
AlexanderRichey <alexander.richey@gmail.com>
2020-01-20 21:27:57 -0800

    truncate user lists

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 import { Box, Text, Paragraph, Avatar } from "./styles";
 
+function getTruncatedUserList(owner, users) {
+  const list = users && users.filter(u => u.id !== owner.id);
+
+  if (list.length > 0) {
+    if (list.length === 1) {
+      return list[0].fullName;
+    } else if (list.length === 2) {
+      return `${list[0].fullName} and ${list[1].fullName}`;
+    } else if (list.length === 3) {
+      return `${list[0].fullName}, ${list[1].fullName}, and ${list.length -
+        2} other`;
+    } else if (list.length > 3) {
+      return `${list[0].fullName}, ${list[1].fullName}, and ${list.length -
+        2} others`;
+    } else {
+      return `${list[0].fullName} and ${list.length - 1} others`;
+    }
+  }
+
+  return "yourself";
+}
+
 export default function ContactSection({ user, users }) {
   return (
     <Box flexDirection="row" alignItems="center" mb={3}>
@@ -15,7 +37,7 @@ export default function ContactSection({ user, users }) {
         </Paragraph>
         <Paragraph mb={0} lineHeight="1em">
           <Text fontSize={1} color="gray" lineHeight="1em">
-            Shared with {users && users.map(u => u.fullName).join(", ")}
+            Shared with {getTruncatedUserList(user, users)}
           </Text>
         </Paragraph>
       </Box>
